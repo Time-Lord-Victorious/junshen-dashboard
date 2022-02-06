d94b3f46d0872c068b7a282db812323f101ecf56
Sherlock <junshenlee2017@gmail.com>
2022-02-06 04:32:42 +0000

    done

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-yellow">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Wesley Studios</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse show" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Who are we?</a>
+          <a class="nav-link" href="#">Productions</a>
+          <a class="nav-link" href="#">Weekly Audio Dramas</a>
+          <a class="nav-link active" aria-current="page" href="#">Customise Account</a>
+        </div>
+      </div>
+    </div>
+  </nav>
---
+++ src/routes/index.svelte
@@ -0,0 +1,47 @@
+<script>
+    import supabase from '$lib/db';
+import Navbar from '$lib/Navbar.svelte';
+    async function logout() {
+   	 const { error } = await supabase.auth.signOut();
+
+   	 if (error) alert(error.message); // alert if error
+    }
+	
+</script>
+<Navbar/>
+
+	<div class="container mt-5">
+		<table class="table">
+		<thead>
+		  <tr>
+			<th scope="col">#</th>
+			<th scope="col">First</th>
+			<th scope="col">Last</th>
+			<th scope="col">Handle</th>
+		  </tr>
+		</thead>
+		<tbody>
+		  <tr>
+			<th scope="row">1</th>
+			<td>Mark</td>
+			<td>Otto</td>
+			<td>@mdo</td>
+		  </tr>
+		  <tr>
+			<th scope="row">2</th>
+			<td>Jacob</td>
+			<td>Thornton</td>
+			<td>@fat</td>
+		  </tr>
+		  <tr>
+			<th scope="row">3</th>
+			<td colspan="2">Larry the Bird</td>
+			<td>@twitter</td>
+		  </tr>
+		</tbody>
+  </table>
+</div>
+<!-- Sign Out -->
+<section class="container px-4 py-3 text-center">
+    <button class="btn btn-secondary" on:click={logout}>Logout</button>
+</section>		
