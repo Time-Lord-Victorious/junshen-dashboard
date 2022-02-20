17db5ef805a06af8459f21276ddf32da805292a6
Sherlock <junshenlee2017@gmail.com>
2022-02-20 02:24:42 +0000

    some changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,21 +26,21 @@ import Navbar from '$lib/Navbar.svelte';
 			<th scope="row">1</th>
 			<td>Doctor Who: Inferno from Space</td>
 			<td>Part 1</td>
-			<td>Jun Shen Lee</td>
+			<td>Junshen</td>
 			<td>1st of May 8pm</td>
 		  </tr>
 		  <tr>
 			<th scope="row">2</th>
 			<td>Doctor Who: Inferno from Space</td>
 			<td>Part 2</td>
-			<td>Jun Shen Lee</td>
+			<td>Junshen</td>
 			<td>15th of May 8pm</td>
 		  </tr>
 		  <tr>
 			<th scope="row">3</th>
 			<td>Doctor Who: Peace and War</td>
 			<td>Part 1</td>
-			<td>Jun Shen Lee</td>
+			<td>Junshen</td>
 			<td>1st of June 8pm</td>
 		  </tr>
 		  <tr>
@@ -54,42 +54,42 @@ import Navbar from '$lib/Navbar.svelte';
 			<th scope="row">5</th>
 			<td>The Ip Man Adventures 1</td>
 			<td>Part 1</td>
-			<td>Jun Shen Lee</td>
+			<td>Junshen</td>
 			<td>1st of July 8pm</td>
 		  </tr>
 		  <tr>
 		  <th scope="row">5</th>
 			<td>The Ip Man Adventures 1</td>
 			<td>Part 2</td>
-			<td>Jun Shen Lee</td>
+			<td>Junshen</td>
 			<td>15th of July 8pm</td>
 		  </tr>
 		  <tr>
 			<th scope="row">6</th>
 			<td>The Ip Man Adventures 2</td>
 			<td>Part 1</td>
-			<td>Jun Shen Lee</td>
+			<td>Junshen</td>
 			<td>1st of August 8pm</td>
 		  </tr>
 		  <tr>
 			<th scope="row">7</th>
 			<td>The Ip Man Adventures 2</td>
 			<td>Part 2</td>
-			<td>Jun Shen Lee</td>
+			<td>Junshen</td>
 			<td>15th of August 8pm</td>
 		  </tr>
 		  <tr>
 			<th scope="row">8</th>
 			<td>The Ip Man Adventures 3</td>
 			<td>Part 1</td>
-			<td>Jun Shen Lee</td>
+			<td>Junshen</td>
 			<td>1st of September 8pm</td>
 		  </tr>
 		  <tr>
 			<th scope="row">8</th>
 			<td>The Ip Man Adventures 3</td>
 			<td>Part 2</td>
-			<td>Jun Shen Lee</td>
+			<td>Junshen</td>
 			<td>15th of September 8pm</td>
 		  </tr>
 		</tbody>
