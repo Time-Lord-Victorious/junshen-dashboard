c44f2d2cc42b374c1e7d3f2b08eb22d21fa994c8
Sherlock <junshenlee2017@gmail.com>
2022-02-20 04:22:38 +0000

    update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -42,10 +42,11 @@ import Navbar from '$lib/Navbar.svelte';
 		<tbody>
 		{#each episodeList as episode}
 		<tr>
-			<td>{episode.name}</td>
-			<td>{episode.part}</td>
-			<td>{episode.author}</td>
-			<td>{episode.date}</td>
+			<td>
+				<button data-bs-toggle="modal" data-bs-target="#editTitle">{episode.name}</button></td>
+			<td><button data-bs-toggle="modal" data-bs-target="#editPart">{episode.part}</button></td>
+			<td><button data-bs-toggle="modal" data-bs-target="#editAuthor">{episode.author}</button></td>
+			<td><button data-bs-toggle="modal" data-bs-target="#editDate">{episode.date}</button></td>
 		  </tr>
 		{/each}
 		<tr>
@@ -56,5 +57,93 @@ import Navbar from '$lib/Navbar.svelte';
 </div>
 <!-- Sign Out -->
 <section class="container px-4 py-3 text-center">
-    <button class="btn btn-secondary" on:click={logout}>Logout</button>
+    <button class="btn btn-primary" on:click={logout}>Logout</button>
 </section>		
+
+  <!-- Modal -->
+<div class="modal fade" id="editTitle" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+	  <div class="modal-content">
+		<div class="modal-header">
+		  <h5 class="modal-title" id="exampleModalLabel">Edit Information</h5>
+		  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		</div>
+		<div class="modal-body">
+			<div class="input-group mb-3">
+				<span class="input-group-text" id="basic-addon1">New title</span>
+				<input type="text" class="form-control" placeholder="New Title" aria-label="New Title" aria-describedby="basic-addon1">
+			  </div>
+		</div>
+		<div class="modal-footer">
+		  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+		  <button type="button" class="btn btn-primary">Save changes</button>
+		</div>
+	  </div>
+	</div>
+  </div>
+  
+ <!-- Modal -->
+ <div class="modal fade" id="editPart" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+	  <div class="modal-content">
+		<div class="modal-header">
+		  <h5 class="modal-title" id="exampleModalLabel">Edit Information</h5>
+		  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		</div>
+		<div class="modal-body">
+			<div class="input-group mb-3">
+				<span class="input-group-text" id="basic-addon1">New Part</span>
+				<input type="text" class="form-control" placeholder="New Part" aria-label="New Part" aria-describedby="basic-addon1">
+			  </div>
+		</div>
+		<div class="modal-footer">
+		  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+		  <button type="button" class="btn btn-primary">Save changes</button>
+		</div>
+	  </div>
+	</div>
+  </div>
+
+   <!-- Modal -->
+<div class="modal fade" id="editAuthor" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+	  <div class="modal-content">
+		<div class="modal-header">
+		  <h5 class="modal-title" id="exampleModalLabel">Edit Information</h5>
+		  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		</div>
+		<div class="modal-body">
+			<div class="input-group mb-3">
+				<span class="input-group-text" id="basic-addon1">New Author</span>
+				<input type="text" class="form-control" placeholder="New Author" aria-label="New Author" aria-describedby="basic-addon1">
+			  </div>
+		</div>
+		<div class="modal-footer">
+		  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+		  <button type="button" class="btn btn-primary">Save changes</button>
+		</div>
+	  </div>
+	</div>
+  </div>
+
+   <!-- Modal -->
+<div class="modal fade" id="editDate" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+	  <div class="modal-content">
+		<div class="modal-header">
+		  <h5 class="modal-title" id="exampleModalLabel">Edit Information</h5>
+		  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		</div>
+		<div class="modal-body">
+			<div class="input-group mb-3">
+				<span class="input-group-text" id="basic-addon1">New Date</span>
+				<input type="text" class="form-control" placeholder="New Date" aria-label="New Date" aria-describedby="basic-addon1">
+			  </div>
+		</div>
+		<div class="modal-footer">
+		  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+		  <button type="button" class="btn btn-primary">Save changes</button>
+		</div>
+	  </div>
+	</div>
+  </div>
